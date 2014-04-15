fb047ea49e239586c9d86b3b2fcc80259cb63fa2
Hannu Haahti <grejppi@gmail.com>
2014-04-16 00:58:52 +0300

    gpl stuff

--- src/eg.c
+++ src/eg.c
@@ -1,4 +1,26 @@
 
+/*
+ * eg.c
+ * 
+ * Copyright (c) 2014 Hannu Haahti <grejppi@gmail.com>
+ * Based on an example by mystran: <http://is.gd/etoZHj>
+ * 
+ * This file is part of Bytes.
+ *
+ * Bytes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Bytes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Bytes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 
 #include "eg.h"
